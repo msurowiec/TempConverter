601236bd440980f1115bd9b979515fa1c3bdc3c0
Matthew-Surowiec <matt@hfft.co>
2026-04-10 13:51:04 -0400

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by Matthew Surowiec on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
